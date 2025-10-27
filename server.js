1b7f4f5db35501568a6531c5104518eaf9f4e9e2
JiahaoFang77 <jiahaof3@illinois.edu>
2025-10-27 01:58:13 -0500

    first trial

--- server.js
+++ server.js
@@ -13,12 +13,8 @@ var app = express();
 // Use environment defined port or 3000
 var port = process.env.PORT || 3000;
 
-// Connect to a MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
-mongoose.connection.once('open', function() {
-    console.log('Successfully connected to MongoDB Atlas...');
-});
+// Connect to a MongoDB --> Uncomment this once you have a connection string!!
+mongoose.connect(process.env.MONGODB_URI,  { useNewUrlParser: true });
 
 // Allow CORS so that backend and frontend could be put on different servers
 var allowCrossDomain = function (req, res, next) {
